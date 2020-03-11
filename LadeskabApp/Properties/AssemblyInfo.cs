bf4a590affbca48fd8e39e316e285adcf45fe37b
Thanh Tieu Pham <au479218@uni.au.dk>
2020-03-11 15:52:45 +0100

    Added new project and solution

--- LadeskabApp/Properties/AssemblyInfo.cs
+++ LadeskabApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ladeskab.Test.Unit")]
+[assembly: AssemblyTitle("LadeskabApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ladeskab.Test.Unit")]
+[assembly: AssemblyProduct("LadeskabApp")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("84f3ed99-b082-4566-a201-bff959e08d48")]
+[assembly: Guid("5549c258-31e0-4eba-a15e-deceec9f8c27")]
 
 // Version information for an assembly consists of the following four values:
 //
