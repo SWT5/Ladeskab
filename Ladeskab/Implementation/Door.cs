72c15ae99e4e42deb2b0d29bf2b67db0659d1f2a
Mads Bo Mikkelsen <au615041@uni.au.dk>
2020-03-17 10:58:45 +0100

    Add Interface IDoor & implementation of Door with simulation methods in Door

---
+++ Ladeskab/Implementation/Door.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ladeskab.Interfaces;
+
+namespace Ladeskab.Implementation
+{
+    public class Door : IDoor
+    {
+        public bool DoorState { get; private set; } 
+
+        public event EventHandler DoorOpenEvent;
+
+        public event EventHandler DoorCloseEvent;
+
+        public void LockDoor()
+        {
+            Console.WriteLine("Door: Locked");
+        }
+
+        public void UnlockDoor()
+        {
+            Console.WriteLine("Door: unlocked");
+        }
+
+        protected virtual void OnDoorOpened()
+        {
+            DoorOpenEvent?.Invoke(this,null);
+        }
+
+        protected virtual void OnDoorClosed()
+        {
+            DoorCloseEvent?.Invoke(this,null);
+        }
+
+        // simuleringer til at klade event DoorOpenEvent
+        public void SimulateDoorOpens()
+        {
+            Console.WriteLine("User Open door");
+            DoorState = true;
+            OnDoorOpened();
+        }
+
+        // simulering til at kalde event DoorCloseEvent
+        public void SimulateDoorCloses()
+        {
+            Console.WriteLine("User closes door");
+            DoorState = false;
+            OnDoorClosed();
+        }
+
+        
+    }
+}
---
+++ Ladeskab/Interfaces/IDoor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladeskab.Interfaces
+{
+    public interface IDoor
+    {
+        bool DoorState { get; }
+
+        event EventHandler DoorOpenEvent;
+        event EventHandler DoorCloseEvent;
+
+        void LockDoor();
+        void UnlockDoor();
+    }
+}
