6e3f1434f08b5b0742bbecb5464d1c5f8b62bf12
Thanh Tieu Pham <au479218@uni.au.dk>
2020-03-18 22:50:39 +0100

    added connection tests to testchargecontrole

--- Ladeskab.Test.Unit/TestChargeControl.cs
+++ Ladeskab.Test.Unit/TestChargeControl.cs
@@ -15,30 +15,65 @@ namespace Ladeskab.Test.Unit
         private ChargeControl uut_;
         public bool eventCalled { get; set; }
         private IUsbCharger usbCharger_;        //Substitute: Fake
+
+
         
         [SetUp]
         public void Setup()
         {
-            
             usbCharger_ = Substitute.For<IUsbCharger>();
             uut_ = new ChargeControl(usbCharger_);
         }
 
+        /***    charging test     ***/
         [Test]
         public void startCharging()
         {
             //Act
             uut_.StartCharge();
             //Assert
-            usbCharger_.Received(1).StartCharge();
-
+            usbCharger_.Received(1).StartCharge();      // received 1 call for startCharge
         }
-
+        
         [Test]
         public void stopCharging()
         {
             uut_.StopCharge();
             usbCharger_.Received(1).StopCharge();       // received 1 call for stopCharge
+        }
+
+        /***    connection test     ***/
+        [Test]
+        public void isConnectedTrue()
+        {
+            uut_.StartCharge();
+            usbCharger_.Connected.Equals(true);
+        }
+        
+        [Test]
+        public void isConnectedFalse()
+        {
+            uut_.StopCharge();
+            usbCharger_.Connected.Equals(false);
+        }
+
+        /***    CurrentState test     ***/
+        [Test]
+        public void CurrentValue_FiveHundred()
+        {
+
+        }
+
+        [Test]
+        public void CurrentValue_two_And_a_Half()
+        {
+
+        }
+
+        [Test]
+        public void overloaded()
+        {
+
         }
     }
 }
