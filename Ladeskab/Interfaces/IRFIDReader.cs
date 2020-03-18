484ffe3727c5dfb2e70d4e085d8b75b5d75c7831
Nikolaj Tam Binh Huynh <au591235@uni.au.dk>
2020-03-18 08:40:28 +0100

    Implemented first iteration of RFIDReader and IRFIDReader, included the eventargs

---
+++ Ladeskab/Interfaces/IRFIDReader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladeskab.Interfaces
+{
+    public class RfidDetectedEventArgs : EventArgs
+    {
+        public string Id { get; set; } //Property to hold the event state 
+    }
+    public interface IRFIDReader
+    {
+        event EventHandler<RfidDetectedEventArgs> RfidDetectedEvent;
+        void RegisterId(string receivedChar);
+    }
+}
