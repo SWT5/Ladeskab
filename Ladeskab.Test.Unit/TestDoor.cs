2962c9a76b26dbfcee2f4b30a63bdacb487b9a74
Mads Bo Mikkelsen <au615041@uni.au.dk>
2020-03-18 17:58:25 +0100

    testDoor bit more implementeted

--- Ladeskab.Test.Unit/TestDoor.cs
+++ Ladeskab.Test.Unit/TestDoor.cs
@@ -4,19 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Ladeskab.Implementation;
+using Ladeskab.Interfaces;
+using NSubstitute;
 using NUnit.Framework;
+
+
+
 
 namespace Ladeskab.Test.Unit
 {
     class TestDoor
     {
         private Door uut_;
+        public bool eventCalled { get; set; }
+        //private IDoor door_;
 
         [SetUp]
         public void Setup()
         {
+            //door_ = Substitute.For<IDoor>();
             uut_ = new Door();
-            //uut_.UnlockDoor();
+            
         }
 
         // State-Based-Tests door
@@ -32,21 +40,21 @@ namespace Ladeskab.Test.Unit
         public void lockDoor_doorIsLocked_LockState_Equal_true()
         {
             uut_.LockDoor();
-            Assert.That(uut_.LockState,Is.True);
+            Assert.That(uut_.LockState,Is.EqualTo(true));
         }
 
         [Test]
         public void simulateDoorOpens_doorIsOpen_doorState_Equal_false()
         {
             uut_.SimulateDoorOpens();
-            Assert.That(uut_.DoorState,Is.False);
+            Assert.That(uut_.DoorState,Is.EqualTo(false));
         }
 
         [Test]
         public void simulateDoorCloses_doorIsClosed_doorState_equal_true()
         {
             uut_.SimulateDoorCloses();
-            Assert.That(uut_.DoorState,Is.True);
+            Assert.That(uut_.DoorState,Is.EqualTo(false));
         }
 
 
@@ -54,6 +62,7 @@ namespace Ladeskab.Test.Unit
         [Test]
         public void simulateDoorOpens_DoorOpenEvent_isTriggered()
         {
+
             // hvordan tester man lige events og at det bliver fired? 
             uut_.SimulateDoorOpens();
         }
