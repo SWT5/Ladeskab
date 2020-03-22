2e6cdc183829e191cd2ddf90c0eda3eb1c640f90
Nikolaj Tam Binh Huynh <au591235@uni.au.dk>
2020-03-22 10:07:17 +0100

    connected event from RFID

--- Ladeskab.Test.Unit/TestRFIDreader.cs
+++ Ladeskab.Test.Unit/TestRFIDreader.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Ladeskab.Implementation;
+using Ladeskab.Interfaces;
 using NUnit.Framework;
 
 namespace Ladeskab.Test.Unit
@@ -13,6 +14,7 @@ namespace Ladeskab.Test.Unit
     {
         private int eventCount { get; set; } //variable to check if this event has been called 
         private RFIDReader _uut;
+        private string Stringreceived { get; set; }
 
         [SetUp]
         public void Setup()
@@ -20,13 +22,22 @@ namespace Ladeskab.Test.Unit
             _uut = new RFIDReader();
 
             _uut.RfidDetectedEvent += (o, e) => { eventCount++; }; //lambda funktion (anonym funktion) - susanne
+            _uut.RfidDetectedEvent += (o, e) => { Stringreceived = e.Id; };
         }
 
         [Test]
-        public void increment()
+        public void Check_If_event_is_triggering_all_subscribers()
         {
             _uut.RegisterId("hej"); //use method to push an event 
             Assert.That(eventCount, Is.EqualTo(1)); //check if an event has happened 
         }
+
+        [Test]
+        public void IdProperty_Ischanged()
+        {
+            _uut.RegisterId("1");
+            Assert.That(Stringreceived, Is.EqualTo("1"));
+        }
+
     }
 }
---
+++ Ladeskab.Test.Unit/TestStationControl.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Ladeskab.Test.Unit
+{
+    class TestStationControl
+    {
+        private StationControl _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            
+
+        }
+
+
+
+    }
+}
